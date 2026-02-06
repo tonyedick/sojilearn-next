faaa7f3221390e618eef15ed84ae3e021d8cc163
Dick Tonye <djoeton@gmail.com>
2026-02-06 11:08:29 +0100

    core: components

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sojilearn.com",
+        port: "",
+        pathname: "/assets/images/**",
+      },
+    ],
+    unoptimized: false,
+  },
+  experimental: { scrollRestoration: true },
 };
 
 export default nextConfig;
