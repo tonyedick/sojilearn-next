cc8c9f90862fd1e81349641e968685e5a2ad12f2
Dick Tonye <djoeton@gmail.com>
2026-02-11 12:15:11 +0100

    bug fix: add vercel deployment fix

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="tw-flex tw-justify-center tw-items-center tw-min-h-screen">
+      <div className="tw-animate-spin tw-rounded-full tw-h-16 tw-w-16 tw-border-b-2 tw-border-blue-600"></div>
+    </div>
+  );
+}
--- src/components/Layouts/AppLayout.tsx
+++ src/components/Layouts/AppLayout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Footer from "../Footer";
 import { Toaster } from "react-hot-toast";
 import Header from "../Header";
